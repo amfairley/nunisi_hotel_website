aa9fa063a29ad7de69b8f4ac081d1176d53bb950
amfairley <adamfairley1990@gmail.com>
2025-06-10 14:56:23 +0100

    style: collect static

--- staticfiles/js/index.js
+++ staticfiles/js/index.js
@@ -1,29 +1,45 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // About us Carousel Logic //
-    const carousel = document.getElementById('aboutUsCarousel');
-    const prevBtn = document.getElementById('prevBtn');
-    const nextBtn = document.getElementById('nextBtn');
+  // About us Carousel Logic //
+  const carousel = document.getElementById('aboutUsCarousel');
+  const prevBtn = document.getElementById('prevBtn');
+  const nextBtn = document.getElementById('nextBtn');
 
-    const toggleButtons = () => {
-        const activeIndex = Array.from(carousel.querySelectorAll('.carousel-item')).findIndex(item =>
-        item.classList.contains('active')
-        );
-        prevBtn.classList.toggle('d-none', activeIndex === 0);
-        nextBtn.classList.toggle('d-none', activeIndex === 1);
-    };
+  const toggleButtons = () => {
+      const activeIndex = Array.from(carousel.querySelectorAll('.carousel-item-desktop')).findIndex(item =>
+      item.classList.contains('active')
+      );
+      prevBtn.classList.toggle('d-none', activeIndex === 0);
+      nextBtn.classList.toggle('d-none', activeIndex === 1);
+  };
 
-    carousel.addEventListener('slid.bs.carousel', toggleButtons);
-    toggleButtons();
+  carousel.addEventListener('slid.bs.carousel', toggleButtons);
+  toggleButtons();
 
-    // Navbar burger menu hiding functionality
-    const navLinks = document.querySelectorAll('.navbar .nav-link');
-    navLinks.forEach(link => {
-      link.addEventListener("click", function () {
-        const navbarCollapse = new bootstrap.Collapse(
-          document.getElementById("navbarSupportedContent"),
-          { toggle: false }
-        );
-        navbarCollapse.hide();
-      });
+  // About us Mobile Carousel Logic //
+  const carouselMobile = document.getElementById('aboutUsCarouselMobile');
+  const prevBtnMobile = document.getElementById('prevBtnMobile');
+  const nextBtnMobile = document.getElementById('nextBtnMobile');
+
+  const toggleButtonsMobile = () => {
+      const activeIndex = Array.from(carouselMobile.querySelectorAll('.carousel-item-mobile')).findIndex(item =>
+      item.classList.contains('active')
+      );
+      prevBtnMobile.classList.toggle('d-none', activeIndex === 0);
+      nextBtnMobile.classList.toggle('d-none', activeIndex === 1);
+  };
+
+  carouselMobile.addEventListener('slid.bs.carousel', toggleButtonsMobile);
+  toggleButtonsMobile();
+
+  // Navbar burger menu hiding functionality
+  const navLinks = document.querySelectorAll('.navbar .nav-link');
+  navLinks.forEach(link => {
+    link.addEventListener("click", function () {
+      const navbarCollapse = new bootstrap.Collapse(
+        document.getElementById("navbarSupportedContent"),
+        { toggle: false }
+      );
+      navbarCollapse.hide();
     });
+  });
 });
