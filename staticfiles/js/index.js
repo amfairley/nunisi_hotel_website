5460e6a6b1f4ab9cbb4136875f1627810bb0939e
amfairley <adamfairley1990@gmail.com>
2025-06-23 09:57:35 +0100

    JS: remove about us mobile carousel part

--- staticfiles/js/index.js
+++ staticfiles/js/index.js
@@ -15,22 +15,6 @@ document.addEventListener('DOMContentLoaded', function () {
   carousel.addEventListener('slid.bs.carousel', toggleButtons);
   toggleButtons();
 
-  // About us Mobile Carousel Logic //
-  const carouselMobile = document.getElementById('aboutUsCarouselMobile');
-  const prevBtnMobile = document.getElementById('prevBtnMobile');
-  const nextBtnMobile = document.getElementById('nextBtnMobile');
-
-  const toggleButtonsMobile = () => {
-      const activeIndex = Array.from(carouselMobile.querySelectorAll('.carousel-item-mobile')).findIndex(item =>
-      item.classList.contains('active')
-      );
-      prevBtnMobile.classList.toggle('d-none', activeIndex === 0);
-      nextBtnMobile.classList.toggle('d-none', activeIndex === 1);
-  };
-
-  carouselMobile.addEventListener('slid.bs.carousel', toggleButtonsMobile);
-  toggleButtonsMobile();
-
   // Navbar burger menu hiding functionality
   const navLinks = document.querySelectorAll('.navbar .nav-link');
   navLinks.forEach(link => {
--- staticfiles/js/rooms.js
+++ staticfiles/js/rooms.js
@@ -1,13 +1,1 @@
-document.addEventListener('DOMContentLoaded', function () {
-  // Navbar burger menu hiding functionality
-  const navLinks = document.querySelectorAll('.navbar .nav-link');
-  navLinks.forEach(link => {
-    link.addEventListener("click", function () {
-      const navbarCollapse = new bootstrap.Collapse(
-        document.getElementById("navbarSupportedContent"),
-        { toggle: false }
-      );
-      navbarCollapse.hide();
-    });
-  });
-});
+document.addEventListener("DOMContentLoaded",(function(){document.querySelectorAll(".navbar .nav-link").forEach((e=>{e.addEventListener("click",(function(){new bootstrap.Collapse(document.getElementById("navbarSupportedContent"),{toggle:!1}).hide()}))}))}));
